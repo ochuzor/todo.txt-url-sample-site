da509864e30bd34f118aa87de1ab5ae30cf04063
Chinedu Eze <chinedu.eze@magicad.com>
2020-03-07 20:16:17 +0200

    set things up for url exporting

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,13 @@
 import {FuseJsTodoIndexer} from '@ochuzor/todo.txt-indexer';
 import {UrlTodoStore, TodoDb, JsonEncoder, B64Ecoder} from '@ochuzor/todo.txt-store';
+import {UrlTodoExporter} from '@ochuzor/todo.txt-export';
 
 const indexer = new FuseJsTodoIndexer();
 const encoder =  JsonEncoder.FromStringEcoder(new B64Ecoder());
 const store = new UrlTodoStore(window.location, encoder);
+
+const exporter = new UrlTodoExporter(window.location.origin, encoder);
+// console.log(exporter.export([{id: 1, text: 'its a good show @computer'}]))
 
 const db = new TodoDb(indexer, store);
 
@@ -13,5 +17,5 @@ function nextId() {
     return db.getAll().length + 1;
 }
 
-export { nextId };
+export { nextId, exporter };
 export default db;
